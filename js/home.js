dbd81803303d5428a80925980236097d2eac8a9f
arahman225 <upworkabdurrahman@gmail.com>
2024-09-19 06:40:25 +0600

    adding money and validate pin number

--- js/home.js
+++ js/home.js
@@ -8,5 +8,20 @@ document.getElementById('add-money-btn').addEventListener('click', function(even
 console.log(addMoneyField)
     const pinNumber = document.getElementById('input-pin-number').value;
 
-    console.log(addMoneyField, pinNumber)
+    // Wrong way to check the pin number
+    if(pinNumber === '16247'){
+        console.log('Successfully added money')
+        const balance = document.getElementById('balance').innerText; //if input field is, have to use .value or innerText
+        const newBalance = addMoneyField;
+        
+        //==============  convert into number   ===-   -----+++++
+        const balanceNumber = Number(newBalance)
+        const addMoneyNumber = parseFloat(balance)
+        const result = balanceNumber + addMoneyNumber;
+        
+        
+
+    }else{
+        alert('Faild to add money. Please try again later!')
+    }
 })
